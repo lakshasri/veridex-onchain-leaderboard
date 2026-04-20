cb10cf770549bd3773fbb583b6a9e7df4b0978e1
lakshasri <176224579+lakshasri@users.noreply.github.com>
2026-04-21 00:06:22 +0530

    remove unused ganache network config

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -14,11 +14,5 @@ module.exports = {
     localhost: {
       url: "http://127.0.0.1:8545",
     },
-    ganache: {
-      url: process.env.GANACHE_RPC_URL || "http://127.0.0.1:7545",
-      accounts: process.env.GANACHE_PRIVATE_KEY
-        ? [process.env.GANACHE_PRIVATE_KEY]
-        : undefined,
-    },
   },
 };
